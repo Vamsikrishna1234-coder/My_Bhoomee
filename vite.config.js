24455ff038de21045071789e602d01d68c102e51
Yegireddi Vamsi Krishna <yegireddivamsikrishna1@gmail.com>
2025-11-12 14:53:28 +0530

    Deploy build to main

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-  base: "/MyBhoomee/", 
+  plugins: [react(), tailwindcss()],
+  base: "/My_Bhoomee/",  // ⚠️ exact repo name (case-sensitive)
+  build: {
+    chunkSizeWarningLimit: 1000,
+  },
 })
